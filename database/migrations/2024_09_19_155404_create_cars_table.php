5a744629deb748441fcc3afe0ba07e1180892dbc
Amrito Biswas <abiswasmr@gmail.com>
2024-09-19 22:25:13 +0600

    Add cars and rentals table schema

---
+++ database/migrations/2024_09_19_155404_create_cars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('brand',50);
+            $table->string('model',50);
+            $table->integer('year');
+            $table->string('car_type');
+            $table->decimal('daily_rent_price',8,2);
+            $table->boolean('availability');
+            $table->string('image',300);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};

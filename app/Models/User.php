ddeb4a9b10b30b49158630638bd8edd3c3305cd8
Amrito Biswas <abiswasmr@gmail.com>
2024-09-19 23:08:12 +0600

    Add user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name','email','password','role','otp'];
+    protected $attribute = ['otp' => '0'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function isAdmin(){
+        return $this->role === 'admin';
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function isCustomer(){
+        return $this->role === 'customer';
+    }
+
+    public function rentals(){
+        return $this->hasMany(Rental::class);
+    }
 }
